823d271b8da8d30a93ff33365472e1659785ab78
李彬华 <binhua_li@outlook.com>
2019-05-20 22:39:30 +0800

    封装validator组件

---
+++ HTCPlatform.ViewModel/Models/Product/AddProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HTCPlatform.ViewModel.Models.Product
+{
+    public class AddProductViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Describe { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public long CategoryId { get; set; }
+        [Required]
+        public string Logo { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+}
---
+++ HTCPlatform.ViewModel/Validators/Product/UpdateProductViewModelValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HTCPlatform.ViewModel.Validators.Product
+{
+    public  class UpdateProductViewModelValidator
+    {
+    }
+}
